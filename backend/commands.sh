0eb79522a20efe56d75afe0c344c548deeef9862
Kevin Martinez <kkevinmartinez7@gmail.com>
2024-07-12 21:34:25 -0600

    models, forms, managers and admin forms

--- backend/commands.sh
+++ backend/commands.sh
@@ -1,1 +1,8 @@
 pip install django djangorestframework django-cors-headers djangorestframework-simplejwt pyjwt django-environ python-dotenv djoser markdown django-filter 
+
+
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createsuperuser 
+-> admin@example.com -> pass1234
+-> kevin@testingsupergood.com  -> pass1234
