ac200f5d9b867b5531d1489d40ad87030cca1786
Kevin Martinez <kkevinmartinez7@gmail.com>
2024-07-15 10:32:49 -0600

    tailwindcss added to the frontend project

--- backend/commands.sh
+++ backend/commands.sh
@@ -1,7 +1,12 @@
+python -m venv venv
 pip install django djangorestframework django-cors-headers djangorestframework-simplejwt djoser pyjwt django-environ python-dotenv markdown django-filter 
 
+django-admin startproject auth_system .
+python manage.py startapp accounts
 
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser 
+
+# testing proposal
+python manage.py createsuperuser
 -> admin@example.com -> pass1234
