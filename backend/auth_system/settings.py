520513306191594408919545dc8356f3d1bed9a2
Kevin Martinez <kkevinmartinez7@gmail.com>
2024-07-14 18:16:54 -0600

    smtp config and testing djoser endpoints

--- backend/auth_system/settings.py
+++ backend/auth_system/settings.py
@@ -176,3 +176,13 @@ DJOSER = {
         "user_delete": "djoser.serializers.UserDeleteSerializer",
     },
 }
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_USE_TLS = True
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
+DOMAIN = env("DOMAIN")
+SITE_NAME = env("APP_NAME")
